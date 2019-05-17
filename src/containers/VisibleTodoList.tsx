2704b03ea722dcb617279bf8b6bdb27b2e109685
hewking <604910070@qq.com>
2019-05-17 19:10:42 +0800

    add todo

--- src/containers/VisibleTodoList.tsx
+++ src/containers/VisibleTodoList.tsx
@@ -1,7 +1,8 @@
 import {connect} from 'react-redux'
 import {toggleTodo} from '../actions'
+import { Todo } from '../reducers';
 
-const getVisibleTodos = (todos : [],filter : string) => {
+const getVisibleTodos = (todos : Todo[],filter : string) => {
     switch(filter) {
         case 'SHOW_COMPLETED' : 
         return todos.filter(t => t.completed)
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,5 +3,9 @@ import {combineReducers} from 'redux'
 import todos from './todos'
 const todoApp = combineReducers(todos)
 
+export interface Todo {
+    completed : boolean
+}
+
 
 export default todoApp
