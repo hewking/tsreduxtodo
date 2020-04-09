87b5177d8e01a9444a544bc38a23f2eba84b6eab
hewking <cjh1095358798@gmail.com>
2020-04-09 15:01:25 +0800

    add scss

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import VisibleTodoList from './containers/VisibleTodoList'
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-function App(){
+function App() {
 
   return (
-    <div style={{display:'flex'
-    ,flex:1
-    ,justifyContent:'center'
-    ,alignItems:'center'
-    ,flexDirection:'column'}}>
-      <Header/>
-      <AddTodo/>
-      <VisibleTodoList/>
-      <Footer/>
+    <div style={{
+      display: 'flex'
+      , flex: 1
+      , justifyContent: 'center'
+      , alignItems: 'center'
+      , flexDirection: 'column'
+    }}>
+      <Header />
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import './Header.scss';
 export default function TitleBar({ title = 'React App' }: { title?: string }) {
     return (<div>
-        <h3>{title}</h3>
+        <h3 className="text">{title}</h3>
     </div>);
 }
